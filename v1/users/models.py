77878db57996d5e9c8f2577ebfefa54a8433fb05
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-27 16:37:10 +0545

    memo for every user

--- v1/users/models.py
+++ v1/users/models.py
@@ -1,4 +1,4 @@
-import uuid
+from uuid import uuid4
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -6,7 +6,8 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    uuid = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+    memo = models.CharField(max_length=44, default="tnbcrow-"+str(uuid4), unique=True,  editable=False)
