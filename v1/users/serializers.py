067fb6145b293112e09de0e009e1495af34e0fc8
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-03-12 09:50:00 +0545

    logic to add and change primary status of wallets

--- v1/users/serializers.py
+++ v1/users/serializers.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+
 from rest_framework import serializers
 
 from .models import Wallet
@@ -9,6 +11,14 @@ class WalletCreateSerializer(serializers.ModelSerializer):
         model = Wallet
         fields = ('uuid', 'account_number', 'is_primary', 'created_at', 'updated_at')
         read_only_fields = 'created_at', 'updated_at', 'is_primary',
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        instance = super(WalletCreateSerializer, self).create(validated_data)
+        if not Wallet.objects.filter(owner=self.context['request'].user, is_primary=True).exists():
+            instance.is_primary = True
+            instance.save()
+        return instance
 
 
 class WalletUpdateSerializer(WalletCreateSerializer):
@@ -17,3 +27,18 @@ class WalletUpdateSerializer(WalletCreateSerializer):
         model = Wallet
         fields = ('uuid', 'account_number', 'is_primary', 'created_at', 'updated_at')
         read_only_fields = 'created_at', 'updated_at'
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        data =self.context['request'].data
+        if 'is_primary' in data:
+            if data['is_primary'] == 'True':
+                primary_wallets = Wallet.objects.filter(owner=self.context['request'].user, is_primary=True)
+                for wallets in primary_wallets:
+                    wallets.is_primary = False
+                    wallets.save()
+            else:
+                validated_data.pop('is_primary', [])
+                raise serializers.ValidationError("You cannot unset Primary Account Number")
+        instance = super(WalletUpdateSerializer, self).update(instance, validated_data)
+        return instance
