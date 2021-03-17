3ab339509646b873ac098bfc73d04cf1f0448aa6
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-03-17 18:46:58 +0545

    added wallet factory

--- v1/users/factories/user.py
+++ v1/users/factories/user.py
@@ -2,7 +2,9 @@ import factory
 from factory.django import DjangoModelFactory
 
 from v1.third_party.tnbCrow.constants import MAX_POINT_VALUE
-from ..models import User
+from v1.third_party.tnbCrow.constants import VERIFY_KEY_LENGTH
+
+from ..models import User, Wallet
 
 
 class UserFactory(DjangoModelFactory):
@@ -14,3 +16,12 @@ class UserFactory(DjangoModelFactory):
 
     class Meta:
         model = User
+
+
+class WalletFactory(DjangoModelFactory):
+    owner = factory.SubFactory(UserFactory)
+    account_number = factory.Faker('pystr', max_chars=VERIFY_KEY_LENGTH)
+    is_primary = factory.Faker('pybool')
+
+    class Meta:
+        model = Wallet
