c29e6a2e4d312404cde483b4d12ad11cdee373cf
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-04-22 13:52:57 +0545

    code: Thread list cannot be accessed by the other users

--- v1/thread/urls.py
+++ v1/thread/urls.py
@@ -6,4 +6,5 @@ app_name = 'thread'
 urlpatterns = [
     path('', ThreadView.as_view({'get': 'list'}), name='thread_list'),
     path('<uuid:thread_id>/', ThreadView.as_view({'get': 'retrieve'}), name='thread_detail'),
+    path('new/', ThreadView.as_view({'post': 'create'}), name='thread_create')
 ]
