1e688becd8551b7f815aa9c2c6fd7e7fbe502195
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-05-08 07:46:41 +0545

    feat: chain scan

--- v1/payments/views.py
+++ v1/payments/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+from .models import ChainScanTracker
+
 PV_IP = "54.219.183.128"
 BANK_IP = "54.177.121.3"
 ESCROW_WALLET = "22d0f0047b572a6acb6615f7aae646b0b96ddc58bfd54ed2775f885baeba3d6a"
@@ -18,10 +20,17 @@ class ChainScan(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
-        r = requests.get(TRANSACTION_URL).json()
-        for transaction in r['results']:
-            transaction_time = timezone.make_aware(datetime.strptime(transaction['block']['modified_date'], '%Y-%m-%dT%H:%M:%S.%fZ'))
-            if timezone.now() > transaction_time:
-                print("Yeah")
-                break
+        scan_tracker = ChainScanTracker.objects.get(id=1)
+        next_url = TRANSACTION_URL
+        while next_url:
+            r = requests.get(TRANSACTION_URL).json()
+            next_url = r['next']
+            for transaction in r['results']:
+                transaction_time = timezone.make_aware(datetime.strptime(transaction['block']['modified_date'], '%Y-%m-%dT%H:%M:%S.%fZ'))
+                if scan_tracker.updated_at < transaction_time:
+                    print(transaction['memo'])
+                    print(transaction['amount'])
+                else:
+                    next_url = None
+                    break
         return Response(status=status.HTTP_201_CREATED)
